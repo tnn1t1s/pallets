cfc0505e7595d536feb800e269ac1866c29fbf78
Jms Dnns <jdennis@gmail.com>
2023-11-20 12:36:27 -0500

    Replaced the model specific training loops with a generalized form. A special case exists in the beginning of the train function to handle transposing an image loaded with a different structure than what pytorch wants. That special case will be removed when repo moves to pillow

--- pallets/models.py
+++ pallets/models.py
@@ -4,6 +4,12 @@ import torch.nn as nn
 import torch.optim as optim
 
 
+# make 4 models:
+# - naive linear + rgb
+# - naive linear + one hot
+# - simple conv  + rgb
+# - simple conv  + one hot
+
 class OneHotAutoencoder(nn.Module):
     def __init__(self):
         super(OneHotAutoencoder, self).__init__()
@@ -29,50 +35,6 @@ class OneHotAutoencoder(nn.Module):
         return x
 
 
-def train_onehot(model, train_loader, test_loader, epochs=5):
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-    train_losses = []
-    test_losses = []
-
-    for epoch in range(epochs):
-        batch_losses = []
-
-        for data in train_loader:
-            inputs = data
-
-            outputs = model(inputs)
-            loss = criterion(outputs, inputs)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            batch_losses.append(np.array(loss.item()))
-
-        batch_loss = np.array(batch_losses).mean(axis=0)
-        print(f'Epoch [{epoch+1}/{epochs}]')
-        print(f'  - train loss: {batch_loss}')
-        train_losses.append(batch_loss)
-
-        model.eval()
-        with torch.no_grad():
-            batch_losses = []
-
-            for data in test_loader:
-                inputs = data
-
-                reconstruction = model(inputs)
-                loss = criterion(reconstruction, inputs)
-                batch_losses.append(np.array(loss.item()))
-
-            batch_loss = np.array(batch_losses).mean(axis=0)
-            print(f"  - test loss:  {batch_loss}")
-            test_losses.append(batch_loss)
-
-    return train_losses, test_losses
-
-
 class SimpleConvAutoencoder(nn.Module):
     def __init__(self):
         super(SimpleConvAutoencoder, self).__init__()
@@ -92,29 +54,67 @@ class SimpleConvAutoencoder(nn.Module):
         )
 
     def forward(self, x):
+        # x.permute(0, 3, 1, 2)
         x = self.encoder(x)
         x = self.decoder(x)
+        # x.permute(0, 2, 3, 1)
         return x
 
 
-def train_simple_conv(model, dataloader, epochs=5):
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+def train(
+        model, criterion, train_loader, test_loader, learn_rate=1e-3, epochs=5
+):
+    # Special case for Conv2D
+    if isinstance(model, SimpleConvAutoencoder):
+        # PyTorch's nn.Conv2d expects input in the format:
+        #   (batch_size, channels, height, width)
+        #
+        # Our dataloader uses mpimg, which uses this format:
+        #   (batch_size, height, width, channels)
+        #
+        # Change from: [32, 24, 24, 222]
+        # Change to:   [32, 222, 24, 24]
+        def prepare_inputs(x): return x.permute(0, 3, 1, 2)
+    else:
+        def prepare_inputs(x): return x
+
+    # Main loop
+    optimizer = optim.Adam(model.parameters(), lr=learn_rate)
+    train_losses = []
+    test_losses = []
 
     for epoch in range(epochs):
-        for data in dataloader:
-            # PyTorch's nn.Conv2d expects input in the format:
-            # (batch_size, channels, height, width)
-            # Our dataloader used the format
-            # (batch_size, height, width, channels).
-            #
-            # Change from [32, 24, 24, 222] to [32, 222, 24, 24]
-            inputs = data.permute(0, 3, 1, 2)
+        batch_losses = []
 
+        for data in train_loader:
+            inputs = prepare_inputs(data)
             outputs = model(inputs)
             loss = criterion(outputs, inputs)
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item()}')
+            batch_losses.append(np.array(loss.item()))
+
+        batch_loss = np.array(batch_losses).mean(axis=0)
+        print(f'epoch [{epoch+1}/{epochs}]')
+        print(f'  - train loss: {batch_loss}')
+        train_losses.append(batch_loss)
+
+        model.eval()
+        with torch.no_grad():
+            batch_losses = []
+
+            for data in test_loader:
+                inputs = prepare_inputs(data)
+                reconstruction = model(inputs)
+                loss = criterion(reconstruction, inputs)
+
+                batch_losses.append(np.array(loss.item()))
+
+            batch_loss = np.array(batch_losses).mean(axis=0)
+            print(f"  - test loss:  {batch_loss}")
+            test_losses.append(batch_loss)
+
+    return train_losses, test_losses
