8e7517f767de24fe3365f28f77f6684ca1bc1814
Jms Dnns <jdennis@gmail.com>
2024-01-22 14:57:34 -0500

    Typo in saved_models_dir

--- pallets/paths.py
+++ pallets/paths.py
@@ -3,7 +3,7 @@ import os
 PALLETS_ROOT = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.abspath(os.path.join(PALLETS_ROOT, '..'))
 ARTIFACTS_DIR = os.path.join(PROJECT_ROOT, 'artifacts')
-SAVED_MODELS_DIR = os.path.join(PROJECT_ROOT, 'saves')
+SAVED_MODELS_DIR = os.path.join(PROJECT_ROOT, 'saved')
 
 # https://github.com/tnn1t1s/cpunks-10k
 CPUNKS_ROOT = os.path.abspath(
