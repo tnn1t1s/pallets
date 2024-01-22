ee248be7483d615a067507b2d2756ef0a182b397
Jms Dnns <jdennis@gmail.com>
2024-01-22 14:03:43 -0500

    Added some constants to resolve paths so users don't have to figure them out

---
+++ pallets/paths.py
@@ -0,0 +1,11 @@
+import os
+
+PALLETS_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(PALLETS_ROOT, '..'))
+ARTIFACTS_DIR = os.path.join(PROJECT_ROOT, 'artifacts')
+SAVED_MODELS_DIR = os.path.join(PROJECT_ROOT, 'saves')
+
+# https://github.com/tnn1t1s/cpunks-10k
+CPUNKS_ROOT = os.path.abspath(
+    os.path.join(PROJECT_ROOT, '..', 'cpunks-10k', 'cpunks')
+)
